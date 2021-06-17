90a5145f0dcc8594335f2fd2ec09bdeaa77fd042
samuelroland <47849646+samuelroland@users.noreply.github.com>
2021-06-18 01:33:21 +0200

    popup: import big code part for data extraction and calculations

--- extension/src/components/Popup.vue
+++ extension/src/components/Popup.vue
@@ -17,14 +17,70 @@
 -->
 <template>
   <div
-    class="bg-blue-300 w-80 overflow-hidden overscroll-none p-1"
-    style="min-height: 75px"
+    class="w-80 overflow-hidden overscroll-none p-1"
+    style="min-height: 75px; color: #76bfff; background-color: #0b0e44;"
   >
-    Workhours
+    <div class="flex flex-1 min-w-max">
+      <h3
+        class="flex flex-row items-center m-0 text-lg cursor-help"
+        title="Internship logbook helper for CPNV students"
+      >
+        logbookHelper
+      </h3>
+      <span
+        class="text-xs cursor-help italic flex items-end ml-1 w-10"
+        :title="'Released the ' + versionDate + '.'"
+        >{{ version }}</span
+      >
+    </div>
+
+    <!-- Extension body -->
+    <div v-if="settingsEnabled">settings...</div>
+    <div v-else>
+      stats...
+      <br />
+      <span v-for="entry in entries" :key="entry.text">{{ entry.text }}</span>
+    </div>
+
+    <!-- Footer with 2 icons: link to source code and settings button - Always displayed -->
+    <div class="flex flex-1 mt-2 justify-end">
+      <img
+        src="icons/github.svg"
+        class="w-7 hover:bg-blue-500 rounded hover:text-white p-1"
+        alt="code icon"
+        title="Checkout the code on GitHub, it's opensource !"
+        @click="goToSourceCode"
+      />
+      <img
+        src="icons/settings.svg"
+        alt="settings icon"
+        title="Open/Close settings"
+        class="w-7 hover:bg-blue-500 rounded hover:text-white px-1"
+        :class="{ 'bg-blue-400': settingsEnabled }"
+        @click="openSettings"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+const startTime = 'style="display: table-cell;" width="30px">';
+const endTime = " h.</td>";
+const startDescription = 'colspan="2" style="display: table-cell;">';
+const endDescription = "</td>";
+const startDate = '<input type="hidden" name="date" value="';
+const endDate = '">';
+const leaveDates = [
+  "2021-05-21",
+  "2021-05-24",
+  "2021-05-25",
+  "2021-05-26",
+  "2021-05-27",
+  "2021-05-28",
+  "2021-04-05",
+  "2021-04-02"
+];
+
 export default {
   name: "Popup",
   props: {
@@ -33,9 +89,118 @@ export default {
     versionDate: String
   },
   data() {
-    return {};
+    return {
+      settingsEnabled: false,
+      entries: null,
+      pageRawContent: null
+    };
   },
   computed: {},
-  methods: {}
+  methods: {
+    //Go to source code button (icon with anchor) on GitHub
+    goToSourceCode() {
+      window.open("https://github.com/samuelroland/logbookHelper", "_blank");
+    },
+    //Open settings and focus on create link input
+    openSettings() {
+      this.settingsEnabled = !this.settingsEnabled; //invert settings status
+    },
+    extractLogbookData() {
+      //logbookhelper.test/raw.html
+
+      var html = this.pageRawContent;
+
+      var ignoredDays = [];
+
+      var currentStartPosDesc = 0;
+      var pos1 = 0;
+      var pos2 = 0;
+      var pos3 = 0;
+      var desc = "";
+      var date = null;
+      var hours = null;
+      var missingHours = null;
+      var lastDate = null;
+
+      var count = 0;
+      var totaltime = 0;
+      var timeperday = 0;
+      var array = [];
+      var nbDays = 0;
+      while (html.indexOf(startDescription, currentStartPosDesc) != -1) {
+        console.log(html.indexOf(startDescription, currentStartPosDesc));
+        if (html.indexOf(startDescription, currentStartPosDesc) != -1) {
+          pos1 =
+            html.indexOf(startDescription, currentStartPosDesc) +
+            startDescription.length;
+          console.log(pos1);
+          //logIt(pos1)
+          desc = html.substr(
+            pos1,
+            html.indexOf(endDescription, pos1 - 1) - pos1
+          );
+          currentStartPosDesc = pos1 + 1;
+
+          pos2 =
+            html.indexOf(startTime, pos1 - (200 + startTime.length)) +
+            startTime.length;
+          //logIt(pos2)
+          hours = html.substr(pos2, html.indexOf(endTime, pos2) - pos2);
+          //console.log(hours)
+
+          pos3 = html.indexOf(startDate, pos1 - 600) + startDate.length;
+          //logIt(pos3)
+          date = html.substr(pos3, html.indexOf(endDate, pos3) - pos3 - 9);
+          //console.log(date)
+
+          if (
+            leaveDates.indexOf(date) == -1 &&
+            desc.indexOf("Cours matu") == -1 &&
+            desc.indexOf("Matu.") == -1 &&
+            desc.indexOf("A remplir") == -1 &&
+            desc != "" &&
+            date.length <= 10
+          ) {
+            totaltime += parseFloat(hours);
+            if (lastDate != date) {
+              console.log("new day: " + date);
+              nbDays++;
+            }
+            lastDate = date;
+            array.push({ text: desc, time: hours, date: date });
+          } else {
+            ignoredDays.push({ text: desc, time: hours, date: date });
+          }
+
+          logIt("date: " + date + " time: " + hours);
+
+          //logIt(timeperweek)
+          console.log(count + " " + currentStartPosDesc + " " + hours);
+          count++;
+        }
+
+        timeperday = totaltime / nbDays;
+        logIt(array);
+        logIt(totaltime + "/" + nbDays * 8.2);
+        logIt(timeperday);
+        missingHours = (8.2 - timeperday) * nbDays;
+        logIt(missingHours);
+        logIt(missingHours / 8);
+
+        this.entries = array;
+      }
+    }
+  },
+  mounted() {
+    sendRequest(
+      "GET",
+      "http://logbookhelper.test/raw.html",
+      this.extractLogbookData,
+      null
+    );
 };
+//Just log text in the console
+function logIt(text) {
+  console.log(text);
+}
 </script>
